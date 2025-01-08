caca3805f5af639e96da08e8cb885e9d10bf2272
Nsengiyumva Wilberforce <nsengiyumvawilberforce@gmail.com>
2025-01-08 14:20:10 +0300

    comments section update

--- routes/posts.js
+++ routes/posts.js
@@ -78,6 +78,7 @@ module.exports = (io, activeUsers) => {
         .skip((page - 1) * limit)
         .limit(Number(limit))
         .populate("author", "username firstName lastName") // Populate author details
+        .populate("comments.user", "username")
         .sort({ createdAt: -1 }); // Sort by creation date (newest first)
 
       res.status(200).json(posts);
